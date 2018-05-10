38c012cf4034faf25090378903b0a5818e216df7
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-05-10 01:52:18 +0100

    Set up Splash Screen, Camera work started, Music Player started

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject); // Could be done in Start but following Unity Doc example
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        Invoke("LoadFirstLevel", 2f);
+	}
+
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
