9a8071c65a70469cbb1d14ddf4a71b651693c925
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-05-12 17:26:01 +0100

    Player Ship now moves more realistically

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -6,10 +7,20 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
     // Movement related variables
-    [Tooltip("In meters per second")][SerializeField] float xSpeed = 4f;
-    [Tooltip("In meters per second")][SerializeField] float ySpeed = 4f;
+    [Tooltip("In meters per second")][SerializeField] float speed = 10f;
     [Tooltip("In meters")][SerializeField] float xRange = 5f;
     [Tooltip("In meters")][SerializeField] float yRange = 5f;
+
+    // Controls the Pitch of the ship when moving up/down so as to always be shooting forward
+    [Tooltip("How much pitch when moving up/down")][SerializeField] float posPitchFactor = -6f;
+    // Controls the pitch of the ship temp when button is pushed so it looks like it's
+    // going up/down on the nose when moving up/down
+    [SerializeField] float controlPitchFactor = -30f;
+    [SerializeField] float posYawFactor = 6f;
+    [SerializeField] float controlRollFactor = -30f;
+
+    // To move ship around
+    float xThrow, yThrow;
 
     // Use this for initialization
     void Start()
@@ -23,13 +34,42 @@ public class Player : MonoBehaviour {
         HandleMovement();
     }
 
+    /// <summary>
+    /// Handles movement related functions
+    /// </summary>
     private void HandleMovement()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        ProcessRotation();
+        ProcessTranslation();
+    }
+
+    #region Movement
+    /// <summary>
+    /// Changes the rotation of the ship during movement
+    /// </summary>
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * posPitchFactor;
+        float pitchDueToControl = yThrow * controlPitchFactor;
+        float pitch =  pitchDueToPosition + pitchDueToControl;
+
+        
+        float yaw = transform.localPosition.x * posYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    /// <summary>
+    /// Moves the ship
+    /// </summary>
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
         // works out how many meters needed to move in this current frame
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
-        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
         float rawYPos = transform.localPosition.y + yOffset;
 
@@ -39,4 +79,5 @@ public class Player : MonoBehaviour {
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
+    #endregion
 }
