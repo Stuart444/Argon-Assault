abbd10335e807546d887cbcfcfe1ea97d55e1b1f
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-06-24 02:30:50 +0100

    Death FX added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+    bool isDead; // False by Default
+
 	// Use this for initialization
 	void Start () {
-		
+        AddNonTriggerBoxCollider();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
 
     void OnParticleCollision(GameObject other)
     {
-        print("Hit: " + gameObject.name);
 
-        Destroy(gameObject);
+        if (!isDead)
+        {
+            isDead = true;
+            print("Hit: " + gameObject.name);
+            GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+            fx.transform.parent = parent;
+            Destroy(gameObject);
+        }
     }
 }
